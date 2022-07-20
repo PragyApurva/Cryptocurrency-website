2f0a0a4a06afeb40d3b1e09e148a0176c0f979a5
HardKofi <2020mcb1229@iitrpr.ac.in>
2022-07-20 18:11:15 +0530

    Connected news api to the website

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,36 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders = {
+    'X-bingApis-SDK': 'true',
+    'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+    'x-rapidapi-key': '15fdf347f6msh2ed031f37f3814bp1171c4jsn8fdeacbfb569',
+};
+
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+ export const cryptoNewsApi = createApi({
+     reducerPath: 'cryptoNewsApi',
+     baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+      getCryptoNews: builder.query({
+        query: ({newsCategory,count}) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+      }),
+
+    }),
+  });
+
+export const {
+
+    useGetCryptoNewsQuery,
+
+} = cryptoNewsApi;
+
+
+
+
+
+
+
+// export const { useGetCryptoNewsQuery } = cryptoNewsApi;
